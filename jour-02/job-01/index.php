478562976628743b57f3c5559870ccb53a8887de
Victor Pettinati <victorpettinati@gmail.com>
2023-09-07 13:31:31 +0200

    PHP

---
+++ jour-02/job-01/index.php
@@ -0,0 +1,55 @@
+<?php
+
+function connect_to_database() {
+    $host = 'localhost'; 
+    $dbname = 'Ip_official'; 
+    $username = 'root'; 
+    $password = 'Laplateforme.06!'; 
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Erreur de connexion à la base de données : " . $e->getMessage());
+    }
+}
+
+function find_all_students() : array {
+    $pdo = connect_to_database();
+    $query = "SELECT * FROM student";
+    $stmt = $pdo->query($query);
+    $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $students;
+}
+
+$students = find_all_students();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des étudiants</title>
+</head>
+<body>
+    <h1>Liste des étudiants</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Date de naissance</th>
+        </tr>
+        <?php foreach ($students as $student) : ?>
+            <tr>
+                <td><?php echo $student['grade_id']; ?></td>
+                <td><?php echo $student['email']; ?></td>
+                <td><?php echo $student['fullname']; ?></td>
+                <td><?php echo $student['birthdate']; ?></td>
+                <td><?php echo $student['gender']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
