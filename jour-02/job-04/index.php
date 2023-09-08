e0771e8fa4b1548fd5c8f3bf7f39617169b97a29
Victor Pettinati <victorpettinati@gmail.com>
2023-09-08 12:47:59 +0200

    PHP

--- jour-02/job-04/index.php
+++ jour-02/job-04/index.php
@@ -1,32 +1,25 @@
 <?php
 function find_all_students_grades(): array {
-    // Informations de connexion à la base de données
     $host = 'localhost';
     $dbname = 'Ip_official';
     $username = 'root';
     $password = 'Laplateforme.06!';
 
     try {
-        // Connexion à la base de données
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-        // Préparation de la requête pour récupérer les données des étudiants avec leurs promotions
         $stmt = $pdo->prepare("SELECT student.email, student.fullname, grade.name AS grade_name
                                FROM student
                                INNER JOIN grade ON student.grade_id = grade.id");
 
-        // Exécution de la requête
         $stmt->execute();
 
-        // Récupération des résultats sous forme de tableau associatif
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        // Fermeture de la connexion
         $pdo = null;
 
         return $results;
     } catch (PDOException $e) {
-        // En cas d'erreur, retournez une tableau vide avec un message d'erreur
         return [
             'error' => true,
             'message' => 'Une erreur s\'est produite lors de la récupération des données : ' . $e->getMessage(),
@@ -34,7 +27,6 @@ function find_all_students_grades(): array {
     }
 }
 
-// Appel de la fonction pour récupérer les données des étudiants
 $students = find_all_students_grades();
 ?>
 
